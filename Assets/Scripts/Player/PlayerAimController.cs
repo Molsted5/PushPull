8d71785306836e35c968b73706a0b6008dc51be4
Jonas Molsted <jonasmolsted@gmail.com>
2025-10-14 17:33:20 +0200

    more accurate aim cast

--- Assets/Scripts/Player/PlayerAimController.cs
+++ Assets/Scripts/Player/PlayerAimController.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using UnityEngine;
 
 [RequireComponent( typeof( PlayerInputHandler ) )]
@@ -5,7 +6,9 @@ public class PlayerAimController: MonoBehaviour {
     public float maxTurnSpeed = 7.5f;
     public float deadZone = 0.18f;
 
-    Vector3 aimDirection;
+    Vector3 direction;
+    Vector3 heightCorrectedPoint;
+    bool usingStick;
     PlayerInputHandler inputHandler;
 
     void Awake() {
@@ -23,22 +26,32 @@ public class PlayerAimController: MonoBehaviour {
     }
 
     void Update() {
-        RotateTowardMouse(); // only for mouse movement
+        if( !usingStick ) {
+            RotateTowardMouse();
+        }
+        usingStick = false;
     }
 
     void OnMouseLook( Vector2 screenPos ) {
         Ray ray = Camera.main.ScreenPointToRay( screenPos );
-        Plane groundPlane = new Plane( Vector3.up, transform.position.y );
+        Plane groundPlane = new Plane( Vector3.up, transform.position );
         if( groundPlane.Raycast( ray, out float distance ) ) {
             Vector3 point = ray.GetPoint( distance );
-            Vector3 direction = (point - transform.position);
-            direction.y = 0f;
-            //Debug.DrawLine( transform.position, transform.position + aimDirection * 1.8f, Color.red );
-            aimDirection = direction.normalized;
+            heightCorrectedPoint = new Vector3( point.x, transform.position.y, point.z );
+        }
+    }
+
+    void RotateTowardMouse() {
+        direction = heightCorrectedPoint - transform.position;
+        direction.y = 0f;
+        direction = direction.normalized;
+        if( direction != Vector3.zero ) {
+            transform.forward = direction;
         }
     }
 
     void OnStickLook( Vector3 velocity ) {
+        usingStick = true;
         Vector3 flatDir = new Vector3( velocity.x, 0f, velocity.z ).normalized;
         float inputStrength = Mathf.Max( velocity.magnitude - deadZone, 0f );
         float turnSpeed = Mathf.Min( inputStrength * maxTurnSpeed, maxTurnSpeed );
@@ -46,11 +59,6 @@ public class PlayerAimController: MonoBehaviour {
         transform.forward = Vector3.RotateTowards( transform.forward, flatDir, turnSpeed * Time.deltaTime, 0f );
     }
 
-    void RotateTowardMouse() {
-        Vector3 flatDir = new Vector3( aimDirection.x, 0f, aimDirection.z );
-        if( flatDir != Vector3.zero ) {
-            transform.forward = flatDir;
-        }
-    }
+
 }
 
