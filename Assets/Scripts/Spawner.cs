02ae23b7a565b0192cdaf6b674b844a93a99e606
Jonas Molsted <jonasmolsted@gmail.com>
2025-12-21 17:14:11 +0100

    new spawn system

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner: MonoBehaviour {
     public Enemy enemy;
-
+    List<Transform> availableSpawns = new List<Transform>();
     float moveSpeed = 1.8f; float damage = 1f; float attackDistanceTreshold = 0.5f; float timeBetweenAttacks = 1f; float health = 5f; Color color = Color.white;
     LivingEntity playerEntity;
     Coroutine spawnCoroutine;
 
     void Awake() {
-        //new Wave(true, -1, 1.8f, 2.2f, 5, 1f, Color.white)
         playerEntity = GameObject.FindGameObjectWithTag("Player").GetComponent<LivingEntity>();
+        foreach( Transform child in transform ) {
+            if( child.name.StartsWith( "Spawn Point" ) ) {
+                availableSpawns.Add( child );
+            }
+        }
     }
 
     void OnEnable() {
@@ -32,22 +37,54 @@ public class Spawner: MonoBehaviour {
     IEnumerator SpawnCoroutine() {
         yield return new WaitForSeconds( 1f );
 
-        yield return SpawnEnemyCoroutine( 10, 1f );
+        yield return SpawnEnemiesCoroutine( 1 );
 
-        yield return new WaitForSeconds( 1f );
+        yield return new WaitForSeconds( 2f );
+
+        yield return SpawnEnemiesCoroutine( 2, 1.5f );
+
+        yield return new WaitForSeconds( 1.5f );
+
+        yield return SpawnEnemiesCoroutine( 3, 2f );
+
+        yield return new WaitForSeconds( 2f );
+
+        yield return SpawnEnemiesCoroutine( 5, 1.4f );
+
+        yield return new WaitForSeconds( 2f );
+
+        yield return SpawnEnemiesCoroutine( 10, 1.4f );
+
+        yield return new WaitForSeconds( 4f );
+
+        yield return SpawnEnemiesCoroutine( 20, 1.7f );
 
         spawnCoroutine = null;
     }
 
-    IEnumerator SpawnEnemyCoroutine( int amount, float timeBetweenSpawns ) {
+    IEnumerator SpawnEnemiesCoroutine( int amount, float timeBetweenSpawns = 1f, float spawnPointTimeout = 0.9f ) {
         for( int i = 0; i < amount; i++ ) {
-            Enemy spawnedEnemy = Instantiate( enemy, new Vector3( 0, 0.5f, 0 ), Quaternion.identity ) as Enemy;
+            Transform spawnPoint = RandomSpawnPoint( spawnPointTimeout );
+            Enemy spawnedEnemy = Instantiate( enemy, spawnPoint.position, spawnPoint.rotation ) as Enemy;
             spawnedEnemy.SetCharacteristics( moveSpeed, damage, attackDistanceTreshold, timeBetweenAttacks, health, color );
-            // wait except after last spawn
+            // wait after each spawn except for the last one
             if( i < amount - 1 ) {
                 yield return new WaitForSeconds( timeBetweenSpawns );
             }
         }
+    }
+
+    Transform RandomSpawnPoint( float spawnPointTimeout ) {
+        int index = UnityEngine.Random.Range( 0, availableSpawns.Count );
+        Transform spawnPoint = availableSpawns[index];
+        availableSpawns.RemoveAt( index );
+        StartCoroutine( ReturnSpawnAfterDelay( spawnPoint, spawnPointTimeout ) );
+        return spawnPoint;
+    }
+
+    IEnumerator ReturnSpawnAfterDelay( Transform spawnPoint, float spawnPointTimeout ) {
+        yield return new WaitForSeconds( spawnPointTimeout );
+        availableSpawns.Add( spawnPoint );
     }
 
     void OnPlayerDeath( ) {
